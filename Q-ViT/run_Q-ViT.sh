08bb008b6e5ee0d6b5666cd5e6ec4b7d768bd168
lyy <870662963@qq.com>
2023-04-24 16:44:14 +0800

    [modify]: run dist_4bit_tiny_lamb_3e-4_300_84

--- Q-ViT/run_Q-ViT.sh
+++ Q-ViT/run_Q-ViT.sh
@@ -1,16 +1,16 @@
-GPU_ID=4,5,6,7
+GPU_ID=0,1,2,4,5
 
 #Train Q-ViT Deit-S 2/3/4bits:
-CUDA_VISIBLE_DEVICES=${GPU_ID} python -m torch.distributed.launch --master_port=12223 --nproc_per_node=4 --use_env main.py \
-        --model fourbits_deit_base_patch16_224 \
-        --epochs 60 \
+CUDA_VISIBLE_DEVICES=${GPU_ID} python -m torch.distributed.launch --master_port=12223 --nproc_per_node=5 --use_env main.py \
+        --model fourbits_deit_tiny_patch16_224 \
+        --epochs 300 \
         --warmup-epochs 0 \
         --weight-decay 0. \
-        --batch-size 2 \
+        --batch-size 84 \
         --data-path /mnt/cephfs/mixed/dataset/imagenet \
         --lr 3e-4 \
         --repeated-aug \
-        --output_dir ./dist_4bit_base_lamb_3e-4_300_path8 \
+        --output_dir ./dist_4bit_tiny_lamb_3e-4_300_84 \
         --distillation-type hard \
-        --teacher-model deit_base_patch16_224 \
+        --teacher-model deit_tiny_patch16_224 \
         --opt fusedlamb
